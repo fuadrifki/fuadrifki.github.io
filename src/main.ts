f93fa854a233359092015100d88c2821b36705ee
fuadrifki <fuad.rifki98@gmail.com>
2021-12-18 19:24:39 +0700

    add google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
+import 'reflect-metadata'
 import { createApp } from 'vue'
 import router from './router'
 import store from './store'
 import App from './App.vue'
 import './assets/index.scss'
 import './assets/tailwind.css'
+import VueGtag from 'vue-gtag'
 
 const app = createApp(App)
-app.use(router)
-app.use(store)
-app.mount('#app')
+app
+  .use(router)
+  .use(store)
+  .use(
+    VueGtag,
+    {
+      config: { id: 'G-3S225KW01Q' }
+    },
+    router
+  )
+  .mount('#app')
